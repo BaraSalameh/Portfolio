94bbd2323152225f8e791412d18d948872f66784
Bara E. Salameh <barasalameh90@gmail.com>
2025-04-18 02:49:22 +0300

    Updating Login command to become POST

--- Portfolio/Controllers/AccountController.cs
+++ Portfolio/Controllers/AccountController.cs
@@ -6,8 +6,8 @@ namespace Portfolio.Controllers
 {
     public class AccountController : ApiController
     {
-        [HttpGet]
-        public async Task<IActionResult> Login([FromQuery] LoginQuery request) => Ok(await Mediator.Send(request));
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginQuery request) => Ok(await Mediator.Send(request));
 
         [HttpPost]
         public async Task<IActionResult> Register(RegisterCommand request) => Ok(await Mediator.Send(request));
