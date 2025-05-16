93b52d3ce759c11f7c6bec1c42f99df0b4a67b94
Bara E. Salameh <barasalameh90@gmail.com>
2025-05-16 04:09:36 +0300

    Updates on education to handle new education structure

--- Application/Owner/MappingProfiles/EducationMappingProfiles.cs
+++ Application/Owner/MappingProfiles/EducationMappingProfiles.cs
@@ -10,9 +10,19 @@ namespace Application.Owner.MappingProfiles
         public EducationMappingProfiles()
         {
             CreateMap<AddEditEducationCommand, Education>();
-            CreateMap<Education, ELQ_Educations>();
+
+            CreateMap<Education, ELQ_Educations>()
+                .ForMember(dest => dest.Institution, opt => opt.MapFrom(src => src.LKP_Institution))
+                .ForMember(dest => dest.Degree, opt => opt.MapFrom(src => src.LKP_Degree))
+                .ForMember(dest => dest.FieldOfStudy, opt => opt.MapFrom(src => src.LKP_FieldOfStudy));
+            CreateMap<LKP_Institution, ELQ_LKP_Institution>();
+            CreateMap<LKP_Degree, ELQ_LKP_Degree>();
+            CreateMap<LKP_FieldOfStudy, ELQ_LKP_FieldOfStudy>();
+
             CreateMap<LKP_Institution, LKP_ILQ_Response>();
+
             CreateMap<LKP_Degree, LKP_DLQ_Response>();
+
             CreateMap<LKP_FieldOfStudy, LKP_FOSLQ_Response>();
         }
     }
--- Application/Owner/Queries/EducationQueries/EducationListQuery.cs
+++ Application/Owner/Queries/EducationQueries/EducationListQuery.cs
@@ -8,11 +8,31 @@ namespace Application.Owner.Queries.EducationQueries
     public class ELQ_Educations
     {
         public Guid ID { get; set; }
-        public string Institution { get; set; }
-        public string Degree { get; set; }
-        public string FieldOfStudy { get; set; }
+        public ELQ_LKP_Institution Institution { get; set; }
+        public ELQ_LKP_Degree Degree { get; set; }
+        public ELQ_LKP_FieldOfStudy FieldOfStudy { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly? EndDate { get; set; }
         public string? Description { get; set; }
+    }
+
+    public class ELQ_LKP_Institution
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string? Logo { get; set; }
+    }
+
+    public class ELQ_LKP_Degree
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string? Abbreviation { get; set; }
+    }
+
+    public class ELQ_LKP_FieldOfStudy
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
     }
 }
