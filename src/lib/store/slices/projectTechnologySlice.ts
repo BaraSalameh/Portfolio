66dad79ae870a51fc7a2bdd0b285bc3655e2a788
Bara E. Salameh <barasalameh90@gmail.com>
2025-05-21 23:17:33 +0300

    Working on multiVal dropdown

--- src/lib/store/slices/projectTechnologySlice.ts
+++ src/lib/store/slices/projectTechnologySlice.ts
@@ -1,16 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { institutionListQuery } from '@/lib/apis/owner/education/institutionListQuery';
-import { degreeListQuery } from '@/lib/apis/owner/education/degreeListQuery';
-import { fieldOfStudyListQuery } from '@/lib/apis/owner/education/fieldOfStudyListQuery';
 import { userFullInfoQuery } from '@/lib/apis/owner/user/userFullInfoQuery';
-import { educationListQuery } from '@/lib/apis/owner/education/educationListQuery';
-import { addEditEducation } from '@/lib/apis/owner/education/addEditEducation';
-import { deleteEducation } from '@/lib/apis/owner/education/deleteEducation';
-import { EducationFormData } from '@/lib/schemas/educationSchema';
-import { userByUsernameQuery } from '@/lib/apis/client/userBuUsernameQuery';
 import { ProjectTechnologyFormData, TechnologyFormData } from '@/lib/schemas/projectTechnologyScehma';
 import { projectTechnologyListQuery } from '@/lib/apis/owner/projectTechnology/projectTechnologyListQuery';
 import { technologyListQuery } from '@/lib/apis/owner/projectTechnology/technologyListQuery';
+import { addEditDeleteProjectTechnology } from '@/lib/apis/owner/projectTechnology/addEdetDeleteProjectTechnology';
 
 interface ProjectTechnologyState {
     lstProjectTechnologies: ProjectTechnologyFormData[],
@@ -58,6 +51,18 @@ const projectTechnologySlice = createSlice({
             state.lstTechnologies = action.payload;
         })
         .addCase(technologyListQuery.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload as string;
+        })
+        
+        .addCase(addEditDeleteProjectTechnology.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(addEditDeleteProjectTechnology.fulfilled, (state) => {
+            state.loading = false;
+        })
+        .addCase(addEditDeleteProjectTechnology.rejected, (state, action) => {
             state.loading = false;
             state.error = action.payload as string;
         });
