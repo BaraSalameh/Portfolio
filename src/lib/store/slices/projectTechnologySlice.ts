e4e4a1372b0a722ee9b982a5cf17c10934762293
Bara E. Salameh <barasalameh90@gmail.com>
2025-05-22 04:47:11 +0300

    delete project slice

--- src/lib/store/slices/projectTechnologySlice.ts
+++ src/lib/store/slices/projectTechnologySlice.ts
@@ -4,10 +4,7 @@ import { ProjectTechnologyFormData, TechnologyFormData } from '@/lib/schemas/pro
 import { projectTechnologyListQuery } from '@/lib/apis/owner/projectTechnology/projectTechnologyListQuery';
 import { technologyListQuery } from '@/lib/apis/owner/projectTechnology/technologyListQuery';
 import { addEditDeleteProjectTechnology } from '@/lib/apis/owner/projectTechnology/addEdetDeleteProjectTechnology';
-<<<<<<< HEAD
 import { deleteProject } from '@/lib/apis/owner/projectTechnology/deleteProject';
-=======
->>>>>>> 66dad79ae870a51fc7a2bdd0b285bc3655e2a788
 
 interface ProjectTechnologyState {
     lstProjectTechnologies: ProjectTechnologyFormData[],
@@ -69,7 +66,6 @@ const projectTechnologySlice = createSlice({
         .addCase(addEditDeleteProjectTechnology.rejected, (state, action) => {
             state.loading = false;
             state.error = action.payload as string;
-<<<<<<< HEAD
         })
 
         .addCase(deleteProject.pending, (state) => {
@@ -82,8 +78,6 @@ const projectTechnologySlice = createSlice({
         .addCase(deleteProject.rejected, (state, action) => {
             state.loading = false;
             state.error = action.payload as string;
-=======
->>>>>>> 66dad79ae870a51fc7a2bdd0b285bc3655e2a788
         });
     },
 });
