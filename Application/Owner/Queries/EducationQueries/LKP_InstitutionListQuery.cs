ea8369bcbd6fa141736bef650a2edff7dda9537e
Bara E. Salameh <barasalameh90@gmail.com>
2025-06-10 18:44:11 +0300

    Updating LKP_Institution handler to accept pagination

--- Application/Owner/Queries/EducationQueries/LKP_InstitutionListQuery.cs
+++ Application/Owner/Queries/EducationQueries/LKP_InstitutionListQuery.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace Application.Owner.Queries.EducationQueries
 {
-    public class LKP_InstitutionListQuery : IRequest<ListQueryResponse<LKP_ILQ_Response>> { }
+    public class LKP_InstitutionListQuery : ListQuery<LKP_ILQ_Response> { }
 
     public class LKP_ILQ_Response
     {
