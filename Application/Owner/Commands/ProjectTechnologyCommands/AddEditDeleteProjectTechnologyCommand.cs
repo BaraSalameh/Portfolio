3e8baa0ac82c568cd3409de41435af1bd6ae3757
Bara E. Salameh <barasalameh90@gmail.com>
2025-07-02 15:10:04 +0300

    Fixing Project command to allow nullable values

--- Application/Owner/Commands/ProjectTechnologyCommands/AddEditDeleteProjectTechnologyCommand.cs
+++ Application/Owner/Commands/ProjectTechnologyCommands/AddEditDeleteProjectTechnologyCommand.cs
@@ -7,10 +7,10 @@ namespace Application.Owner.Commands.ProjectTechnologyCommands
     {
         public Guid? ID { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
-        public string LiveLink { get; set; }
-        public string SourceCode { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Description { get; set; }
+        public string? LiveLink { get; set; }
+        public string? SourceCode { get; set; }
+        public string? ImageUrl { get; set; }
         public bool IsFeatured { get; set; }
         public Guid? EducationID { get; set; }
         public Guid? ExperienceID { get; set; }
